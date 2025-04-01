157a3d952f0481ca8eabb66bf96cd89a3ee6ad2e
Adrian-glitc <amadridm@student.universidadunie.com>
2025-04-01 16:52:10 +0200

    cambios

--- main.py
+++ main.py
@@ -5,24 +5,3 @@ if __name__ == "__main__":
     receta.procesar_texto()
     print(receta.obtener_resultado())
     
-class Receta:
-    def __init__(self, texto_corrupto):
-        self.texto_corrupto = texto_corrupto
-        self.texto_invertido = ""
-        self.nombre_receta = ""
-        self.calorias = ""
-        
-    def procesar_texto(self):
-            # Invertir la cadena
-            self.texto_invertido = self.texto_corrupto[::-1]
-
-            # Separar el texto en partes
-            partes = self.texto_invertido.split(" ")
-
-            # Extraer el nombre de la receta y las calorías
-            self.nombre_receta = " ".join(partes[3:])  # Unir las palabras que forman el nombre de la receta
-            self.calorias = partes[1]  # Obtener el número de calorías
-        
-    def obtener_resultado(self):
-            # Formatear el texto
-        return f"La receta de {self.nombre_receta} contiene {self.calorias} calorías."
